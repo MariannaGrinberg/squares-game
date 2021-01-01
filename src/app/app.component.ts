d45287b3d2535d0e07e805ff6b1bbe16fa494604
MariannaGrinberg <marianna4268@gmail.com>
2021-01-01 21:53:42 +0200

    Square functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'squares-game';
+
+
+  backgroundColor = this.getColor()
+
+  // getColor
+
+  getColor() {
+
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+
+    return `rgb(${r}, ${g}, ${b})`
+
+  }
+
+  // setColor
+
+  setColor() {
+
+    this.backgroundColor = this.getColor()
+
+  }
+
+  
 }
